9e764867322988833453bffccc9d25493f01df2b
leandermaben <leander.maben@gmail.com>
2022-01-31 13:19:28 +0000

    Added script to prepare audio files

---
+++ datasets/fetchData.py
@@ -0,0 +1,127 @@
+import argparse
+import shutil
+import torch
+import tqdm
+import os
+import numpy as np
+import math
+import cv2
+import librosa
+import pickle
+import matplotlib.pyplot as plt
+
+AUDIO_DATA_PATH_DEFAULT = '/content/drive/MyDrive/NTU - Speech Augmentation/Parallel_speech_data'
+SUBDIRECTORIES_DEFAULT = ['clean','noisy']
+CACHE_DEFAULT = '/content/Pix2Pix-VC/data_cache'
+SAMPLING_RATE = 22050
+
+## mel function inspired from https://github.com/GANtastic3/MaskCycleGAN-VC
+
+def mel(wavspath):
+    info = {
+        'records_count' : 0, #Keep track of number of clips
+        'duration' : 0 # Keep track of duration of training set for the speaker
+    }
+    
+    vocoder = torch.hub.load('descriptinc/melgan-neurips', 'load_melgan')
+
+    mel_list = list()
+    wav_files = os.listdir(wavspath)
+    filenames = []
+    for file in wav_files:
+        wavpath = os.path.join(wavspath,file)
+        if wavpath[-4:] != '.wav':
+            continue
+        wav_orig, _ = librosa.load(wavpath, sr=SAMPLING_RATE, mono=True)
+        spec = vocoder(torch.tensor([wav_orig]))
+        #print(f'Spectrogram shape: {spec.shape}')
+        if spec.shape[-1] >= 64:    # training sample consists of 64 frames
+            mel_list.append(spec.cpu().detach().numpy()[0])
+            info['duration']+=librosa.get_duration(filename=wavpath)
+            info['records_count']+=1   
+            filenames.append(file)       
+
+  
+
+    return mel_list, filenames, info
+
+
+def save_pickle(variable, fileName):
+    with open(fileName, 'wb') as f:
+        pickle.dump(variable, f)
+
+
+def preprocess_dataset(data_path, class_id, args):
+    """Preprocesses dataset of .wav files by converting to Mel-spectrograms.
+    Args:
+        data_path (str): Directory containing .wav files of the speaker.
+        speaker_id (str): ID of the speaker.
+        cache_folder (str, optional): Directory to hold preprocessed data. Defaults to './cache/'.
+    """
+
+    print(f"Preprocessing data for class: {class_id}.")
+
+    cache_folder = args.data_cache
+
+    mel_list, filenames, info = mel(data_path)
+
+    if not os.path.exists(os.path.join(cache_folder, class_id)):
+        os.makedirs(os.path.join(cache_folder, class_id, 'meta'))
+        os.makedirs(os.path.join(cache_folder, class_id, 'train'))
+        os.makedirs(os.path.join(cache_folder, class_id, 'val'))
+        os.makedirs(os.path.join(cache_folder, class_id, 'test'))
+    
+    indices = np.arange(0,len(mel_list))
+    np.random.shuffle(indices)
+
+    train_split = math.floor(args.train_percent/100*len(mel_list))
+    val_split = math.floor(args.val_percent/100*len(mel_list))
+    
+    padding ={}
+
+    for phase,(start, end) in zip(['train','val','test'],[(0,train_split),(train_split,train_split+val_split),(train_split+val_split,len(mel_list))]):
+        for i in range(start,end):
+            filename=filenames[indices[i]]
+            img = mel_list[indices[i]]
+            filename=filename[:33]+'.jpg' ## THIS STEP IS SPECIFIC TO THE CURRENT DATASET TO ENSURE A AND B HAVE SAME FILENAMES
+            
+            ##Padding the image
+            freq_len,time_len = img.shape
+            top_pad = freq_len % args.size_multiple
+            right_pad = time_len % args.size_multiple 
+            x_size = time_len+right_pad
+            y_size = freq_len+top_pad
+            img_padded = np.zeros((y_size,x_size))
+            img_padded[-freq_len:,0:time_len] = img
+
+            ## Saving Padding info
+            padding[filename] = (top_pad,right_pad)
+
+            ##Saving Image
+            cv2.imwrite(os.path.join(cache_folder,class_id,phase,filename),img_padded)
+    
+    save_pickle(variable=padding,fileName=os.path.join(cache_folder, class_id, 'meta', f"{class_id}_padding.pickle"))
+
+    print('#'*25)
+    print(f"Preprocessed and saved data for class: {class_id}.")
+    print(f"Total duration of dataset for {class_id} is {info['duration']} seconds")
+    print(f"Total clips in dataset for {class_id} is {info['records_count']}")
+    print('#'*25)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Generate Spectrograms')
+    parser.add_argument('--audio_data_path', dest = 'audio_path', type=str, default=AUDIO_DATA_PATH_DEFAULT, help="Path to audio root folder")
+    parser.add_argument('--source_sub_directories', dest = 'sub_directories',type=str, default=SUBDIRECTORIES_DEFAULT, help="Sub directories for data")
+    parser.add_argument('--data_cache', dest='data_cache', type=str, default=CACHE_DEFAULT, help="Directory to Store data and meta data.")
+    parser.add_argument('--train_percent', dest='train_percent', type=int, default=70, help="Percentage for train split")
+    parser.add_argument('--val_percent', dest='val_percent', type=int, default=15, help="Percentage for val split")
+    parser.add_argument('--size_multiple', dest='size_multiple', type=int, default=256, help="Required Factor of Dimensions")
+    args = parser.parse_args()
+    for class_id in args.sub_directories:
+        preprocess_dataset(os.path.join(args.audio_path,class_id),class_id,args)
+
+
+
+
